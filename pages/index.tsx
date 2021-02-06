a8f0f38438942a1b96c4c01947b8f6780e44af20
Dev Kahar <devkahar99@gmail.com>
2021-02-06 19:47:47 +0530

    styled JSX

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
+// Styled-JSX
+// This Style is limited to this component only
 export default function Home() {
+  const color = 'yellow';
   return (
+    <div>
+
+      <h1 className="title">Hello World</h1>
+      <p>Hii </p>
+      <style jsx>
+        {`
+          .title{
+            color: red;
+          }
+          p{
+            color: ${color};
+          }
+        `}
+      </style>
+    </div>
     
-    <h1>Hello World</h1>
   )
 }
--- pages/nicepage.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-    return (
-        // localhost:3000/nicepage
-      <h1>Hello Nice Page!</h1>
-    )
-}
